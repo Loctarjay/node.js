15c459cc31911d5da6538d48538f955635665f3d
Loctarjay <jl-p@hotmail.com>
2020-09-08 11:30:52 +0200

    Second Commit

--- 2.Our_First_API/app.js
+++ 2.Our_First_API/app.js
@@ -1,2 +1,24 @@
 // const express = require("express");
 
+// require object.json and console log
+//const object = require("../1._Intro/object.json");
+
+
+
+
+
+const app = require("express")();
+const port = 8080;
+
+
+
+app.get("/", (req, res) => {
+    return res.send();
+});
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log("Error starting the server");
+    }
+    console.log("Server is running on port: ", port);
+});
